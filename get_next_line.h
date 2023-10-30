660f1830aee101707980af3396e3980f08686bd5
Klowsss <klowsdiscord@gmail.com>
2023-10-30 22:09:14 +0100

    dslakda

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: btiza <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 18:46:54 by btiza             #+#    #+#             */
-/*   Updated: 2023/10/30 22:00:57 by btiza            ###   ########.fr       */
+/*   Updated: 2023/10/30 22:09:07 by btiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	ft_strcpy(char *dest, char *src);
-char	*line_extract(dd);
+char	*line_extract(d);
 char	*read_content(int files, void *buff, size_t nbyte);
 char	*get_next_line(int fd);
 
