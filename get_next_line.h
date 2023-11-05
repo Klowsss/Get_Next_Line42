be3d007f6ad8d55defa544c00f21ca171bc6ccbc
Klowsss <144048059+Klowsss@users.noreply.github.com>
2023-11-05 21:56:48 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -13,21 +13,23 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 1
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <fcntl.h>
-# include <stdio.h>
+#  include <fcntl.h>
+#  include <stdio.h>
+#  include <stdlib.h>
+#  include <unistd.h>
+#  include <stdlib.h>
+#  include <stddef.h>
 
-int	ft_strlen(char *str);
+int		ft_strlen(const char *str);
 char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strdup(const char *s);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	ft_strcpy(char *dest, char *src);
-char	*line_extract(d);
-char	*read_content(int files, void *buff, size_t nbyte);
 char	*get_next_line(int fd);
+char    *ft_strchr(const char *s, int c);
+void    ft_bzero(void *p, size_t n);
+void    *ft_calloc(size_t nmemb, size_t size);
+
+# endif
 
 #endif
